d0e71dacd8fdadac4b8463ff3b929aa956e3303d
Hector Alpuche Rodriguez <hector.alprod.2021@gmail.com>
2022-05-28 20:10:12 -0500

    Primer commit para darle seguimiento a los archivos

---
+++ app/src/main/java/com/T0t0t4t0/helloandroidstudio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.T0t0t4t0.helloandroidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
